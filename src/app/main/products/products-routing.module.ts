b62167ffbea534dbb295c07ea1085578d760167f
Алексей Столяров <stolya25@gmail.com>
2023-10-20 19:56:08 +0300

    Rename products module

--- src/app/main/products/products-routing.module.ts
+++ src/app/main/products/products-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { GoodsListComponent } from './goods-list.component';
+import { ProductsComponent } from './products.component';
 
-const routes: Routes = [{ path: '', component: GoodsListComponent }];
+const routes: Routes = [{ path: '', component: ProductsComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class GoodsListRoutingModule { }
+export class ProductsRoutingModule { }
