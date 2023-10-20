461fe8deda0e41f0f4ab60197f8e119a57a11346
Алексей Столяров <stolya25@gmail.com>
2023-10-20 20:02:36 +0300

    Rebase products module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/products', pathMatch: 'full' },
   { path: 'cart', loadChildren: () => import('./cart/cart.module').then(m => m.CartModule) },
   { path: 'search', loadChildren: () => import('./search/search.module').then(m => m.SearchModule) },
-  { path: 'products', loadChildren: () => import('./main/products/products.module').then(m => m.ProductsModule) },
+  { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
   { path: '**', loadChildren: () => import('./error/error.module').then(m => m.ErrorModule) }
 ];
 
