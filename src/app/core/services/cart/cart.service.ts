938fdf6d4121813e54ae5e33bde695c131a1bb51
Алексей Столяров <stolya25@gmail.com>
2023-10-23 12:54:37 +0300

    Add number of products badge

--- src/app/core/services/cart/cart.service.ts
+++ src/app/core/services/cart/cart.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Store} from "@ngrx/store";
-import {Observable, of, switchMap} from "rxjs";
+import {filter, map, Observable, of, switchMap} from "rxjs";
 import {ProductModel} from "../../models/api/product.model";
 import {CartSelectors} from "../../selectors/cart.selectors";
 import {CartActions} from "../../actions/cart.actions";
@@ -33,5 +33,24 @@ export class CartService {
 
   addToCart(product: ProductModel): void {
     this.store.dispatch(CartActions.pushProduct(product));
+  }
+
+  getTotalCost(): Observable<number> {
+    return this.getProducts().pipe(
+      filter(products => products.length > 0),
+      map(products => products.map(product => product.price)),
+      switchMap(prices => {
+        return of(prices.reduce((accum: number, price) => accum + price, 0))
+      })
+    )
+  }
+
+  getTotalProducts(): Observable<number> {
+    return this.getProducts().pipe(
+      filter(products => products.length > 0),
+      switchMap(prices => {
+        return of(prices.reduce((accum: number) => accum + 1, 0))
+      })
+    )
   }
 }
